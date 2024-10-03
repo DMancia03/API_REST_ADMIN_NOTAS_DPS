1a0efcd56c14d872242b8f9913b0e039b3239216
DMancia03 <diego.mancia.03@gmail.com>
2024-10-02 20:46:49 -0600

    Creando DB Context para aplicación

---
+++ Data/AdminNotasContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace API_REST_ADMIN_NOTAS.Data
+{
+    public class AdminNotasContext : DbContext
+    {
+        public AdminNotasContext(DbContextOptions<AdminNotasContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Nota>().ToTable("Notas");
+        }
+    }
+}
