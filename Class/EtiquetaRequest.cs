d32dbf19ef59f58d9eb1330abc16fe9145a66344
DMancia03 <diego.mancia.03@gmail.com>
2024-10-03 22:33:03 -0600

    Agregando clases request de etiquetas y usuario

---
+++ Class/EtiquetaRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_REST_ADMIN_NOTAS.Class
+{
+    public class EtiquetaRequest
+    {
+        public int IdEtiqueta { get; set; }
+        public string Nombre { get; set; } = null!;
+        public int IdUsuario { get; set; }
+    }
+}
---
+++ Class/UserRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API_REST_ADMIN_NOTAS.Class
+{
+    public class UserRequest
+    {
+        public int IdUsuario { get; set; }
+        public string Username { get; set; } = null!;
+        public string Pass { get; set; } = null!;
+    }
+}
