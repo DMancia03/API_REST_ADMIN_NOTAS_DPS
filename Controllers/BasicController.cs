8d40f48325cb94b9d6c58b1042585fcf5e077de1
DMancia03 <diego.mancia.03@gmail.com>
2024-10-03 22:33:21 -0600

    Agregando basic controller

---
+++ Controllers/BasicController.cs
@@ -0,0 +1,10 @@
+﻿using API_REST_ADMIN_NOTAS.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_REST_ADMIN_NOTAS.Controllers
+{
+    public class BasicController : ControllerBase
+    {
+        protected AdminNotasContext _db;
+    }
+}
