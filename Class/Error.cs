14384d6fec35def5600e4649051d7dd9770be773
DMancia03 <diego.mancia.03@gmail.com>
2024-10-03 22:32:48 -0600

    Agregando clases de ayuda

---
+++ Class/Error.cs
@@ -0,0 +1,12 @@
+﻿namespace API_REST_ADMIN_NOTAS.Class
+{
+    public class Error
+    {
+        public Error()
+        {
+            Descripcion = "";
+        }
+
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Class/Session.cs
@@ -0,0 +1,13 @@
+﻿namespace API_REST_ADMIN_NOTAS.Class
+{
+    public class Session
+    {
+        public Session()
+        {
+            Username = "";
+        }
+
+        public int IdUsuario { get; set; }
+        public string Username { get; set; }
+    }
+}
