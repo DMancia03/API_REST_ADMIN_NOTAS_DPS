0a013ceaf6171b7b649e8d13d3ce776940bc2b44
DMancia03 <diego.mancia.03@gmail.com>
2024-10-04 00:05:48 -0600

    Agregando constantes

---
+++ Helpers/Constantes.cs
@@ -0,0 +1,8 @@
+﻿namespace API_REST_ADMIN_NOTAS.Helpers
+{
+    public class Constantes
+    {
+        public const string ESTADO_ACTIVO = "Activo";
+        public const string ESTADO_EN_PAPELERA = "EnPapelera";
+    }
+}
