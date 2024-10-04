3af72e50162ba69bafb7b08a88878c244f8199ca
DMancia03 <diego.mancia.03@gmail.com>
2024-10-03 22:33:12 -0600

    Agregando helper

---
+++ Helpers/Helper.cs
@@ -0,0 +1,34 @@
+﻿using API_REST_ADMIN_NOTAS.Class;
+using API_REST_ADMIN_NOTAS.Models;
+
+namespace API_REST_ADMIN_NOTAS.Helpers
+{
+    public class Helper
+    {
+        public static Session CrearSesion(Usuario user)
+        {
+            return new Session()
+            {
+                IdUsuario = user.IdUsuario,
+                Username = user.Username
+            };
+        }
+
+        public static Error CrearError(string descripcion)
+        {
+            return new Error()
+            {
+                Descripcion = descripcion
+            };
+        }
+
+        public static Usuario NormalizarUsuario(UserRequest user)
+        {
+            return new Usuario()
+            {
+                Username = user.Username,
+                Pass = user.Pass
+            };
+        } 
+    }
+}
