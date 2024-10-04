4f2619172c933cb248e4c57a200a7a33aad4ce6c
DMancia03 <diego.mancia.03@gmail.com>
2024-10-03 22:55:55 -0600

    Agrregando helpers para clases intermediarias entre modelo y respone

--- Helpers/Helper.cs
+++ Helpers/Helper.cs
@@ -5,6 +5,7 @@ namespace API_REST_ADMIN_NOTAS.Helpers
 {
     public class Helper
     {
+        #region Sesion
         public static Session CrearSesion(Usuario user)
         {
             return new Session()
@@ -13,7 +14,9 @@ namespace API_REST_ADMIN_NOTAS.Helpers
                 Username = user.Username
             };
         }
+        #endregion
 
+        #region Error
         public static Error CrearError(string descripcion)
         {
             return new Error()
@@ -21,7 +24,9 @@ namespace API_REST_ADMIN_NOTAS.Helpers
                 Descripcion = descripcion
             };
         }
+        #endregion
 
+        #region Usuario
         public static Usuario NormalizarUsuario(UserRequest user)
         {
             return new Usuario()
@@ -29,6 +34,87 @@ namespace API_REST_ADMIN_NOTAS.Helpers
                 Username = user.Username,
                 Pass = user.Pass
             };
-        } 
+        }
+        #endregion
+
+        #region Etiqueta
+        public static Etiqueta NormalizarEtiqueta(EtiquetaRequest etiq)
+        {
+            return new Etiqueta()
+            {
+                IdEtiqueta = etiq.IdEtiqueta,
+                IdUsuario = etiq.IdUsuario,
+                Nombre = etiq.Nombre
+            };
+        }
+
+        public static EtiquetaRequest CrearEtiquetaRequest(Etiqueta etiqueta)
+        {
+            return new EtiquetaRequest()
+            {
+                IdEtiqueta = etiqueta.IdEtiqueta,
+                IdUsuario = etiqueta.IdUsuario,
+                Nombre = etiqueta.Nombre
+            };
+        }
+        #endregion
+
+        #region Nota
+        public static Nota NormalizarNota(NotaRequest nota)
+        {
+            return new Nota()
+            {
+                IdNota = nota.IdNota,
+                IdUsuario = nota.IdUsuario,
+                IdEtiqueta = nota.IdEtiqueta,
+                Nombre = nota.Nombre,
+                Contenido = nota.Contenido,
+                Estado = nota.Estado
+            };
+        }
+
+        public static NotaRequest CrearNotaRequest(Nota nota)
+        {
+            return new NotaRequest()
+            {
+                IdNota = nota.IdNota,
+                IdUsuario = nota.IdUsuario,
+                IdEtiqueta = nota.IdEtiqueta,
+                Nombre = nota.Nombre,
+                Contenido = nota.Contenido,
+                Estado = nota.Estado
+            };
+        }
+        #endregion
+
+        #region Recordatorio
+        public static Nota NormalizarRecordatorio(RecordatorioRequest recordatorio)
+        {
+            return new Nota()
+            {
+                IdNota = recordatorio.IdNota,
+                IdUsuario = recordatorio.IdUsuario,
+                IdEtiqueta = recordatorio.IdEtiqueta,
+                Nombre = recordatorio.Nombre,
+                Contenido = recordatorio.Contenido,
+                Estado = recordatorio.Estado,
+                FechaRecordatorio = recordatorio.FechaRecordatorio
+            };
+        }
+
+        public static RecordatorioRequest CrearRecordatorioRequest(Nota recordatorio)
+        {
+            return new RecordatorioRequest()
+            {
+                IdNota = recordatorio.IdNota,
+                IdUsuario = recordatorio.IdUsuario,
+                IdEtiqueta = recordatorio.IdEtiqueta,
+                Nombre = recordatorio.Nombre,
+                Contenido = recordatorio.Contenido,
+                Estado = recordatorio.Estado,
+                FechaRecordatorio = recordatorio.FechaRecordatorio
+            };
+        }
+        #endregion
     }
 }
